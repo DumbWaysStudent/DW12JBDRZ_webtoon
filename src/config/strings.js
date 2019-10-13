2665901857abe9368dc5255886f328518cb5338d
Egi Ginting <exneval.rayz@gmail.com>
2019-10-13 07:29:40 +0700

    add create webtoon screen

---
+++ src/config/strings.js
@@ -0,0 +1,30 @@
+const strings = {
+  LOGIN: 'Log In',
+  LOGOUT: 'Log Out',
+  WELCOME_TO_LOGIN1: 'Login',
+  WELCOME_TO_LOGIN2: 'TOON KINGDOM',
+  EMAIL_PLACEHOLDER: 'Email',
+  PASSWORD_PLACEHOLDER: 'Password',
+  EMAIL_REQUIRED: 'Email is required',
+  EMAIL_INVALID: 'Invalid email format',
+  PASSWORD_REQUIRED: 'Password is required',
+  FONT: 'Mansalva-Regular',
+  FAVOURITE: 'Favourite',
+  ALL: 'All',
+  IMAGE1_URL:
+    'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c30c2824-fb37-4b0e-a4c9-d43c3f9e6aee/d3eq6nu-376a8946-9b38-411f-b3bd-4d83fa10248b.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2MzMGMyODI0LWZiMzctNGIwZS1hNGM5LWQ0M2MzZjllNmFlZVwvZDNlcTZudS0zNzZhODk0Ni05YjM4LTQxMWYtYjNiZC00ZDgzZmExMDI0OGIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.O4_aMnTx80Pu0e_iujxChE4Fyo4nDhITaRBxvHFt3uk',
+  IMAGE2_URL:
+    'https://i.pinimg.com/originals/b6/d0/89/b6d08950ecd8e5c93fe564e74ae46dec.png',
+  IMAGE3_URL:
+    'http://pm1.narvii.com/6746/cb154c788fe053e1ccc35da347d803f43c18f403v2_00.jpg',
+  IMAGE4_URL: 'https://i.imgur.com/WysEJFj.jpg',
+  IMAGE5_URL: 'https://i.imgur.com/4Hx2xMP.jpg',
+  IMAGE6_URL: 'https://i.imgur.com/0viGL0O.jpg',
+  CREATE_TOON_PAGE: 'My Toon Kingdom',
+  TITLE: 'Title',
+  EPISODE: 'Episode',
+  ADD_FAVORITE: '+ Favorite',
+  ADD_EPISODE: '+ Episode',
+};
+
+export default strings;
---
+++ src/screens/CreateMyToonScreen.js
@@ -0,0 +1,156 @@
+import React, {Component} from 'react';
+import {
+  Text,
+  StyleSheet,
+  View,
+  TextInput,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+
+import colors from '../config/colors';
+import strings from '../config/strings';
+import metrics from '../config/metrics';
+
+export default class CreateMyToon extends Component {
+  showTitleBar = () => {
+    return (
+      <View style={styles.titleContainer}>
+        <TextInput style={styles.titleBar} />
+      </View>
+    );
+  };
+
+  showListToon = (toon, index) => {
+    return (
+      <View key={index} style={styles.showListContainer}>
+        <View style={styles.listImage}>
+          <Image
+            style={styles.showListImage}
+            source={{
+              uri: toon.imageURI,
+            }}
+          />
+        </View>
+        <View style={styles.listNameContainer}>
+          <Text style={styles.listName}>{toon.title}</Text>
+          <Text style={styles.epsNameDate}>{toon.count} Episode(s)</Text>
+        </View>
+      </View>
+    );
+  };
+
+  showListTitle = toons => {
+    return (
+      <View style={styles.listsContainer}>
+        {toons.map((toon, index) => this.showListToon(toon, index))}
+      </View>
+    );
+  };
+
+  renderSub = () => {
+    const {params} = this.props.navigation.state;
+
+    return (
+      <View style={styles.createToonContainer}>
+        <Text style={styles.titleText}>{strings.TITLE}</Text>
+        {this.showTitleBar()}
+        <Text style={styles.titleText}>{strings.EPISODE}</Text>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          {this.showListTitle(params)}
+        </ScrollView>
+        <View style={styles.addEpsBtnContainer}>
+          <TouchableOpacity style={styles.addEpsBtn}>
+            <Text style={styles.addEpsBtnText}>{strings.ADD_EPISODE}</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.content}>{this.renderSub()}</View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.WHITE,
+    alignItems: 'center',
+  },
+  content: {
+    flex: 1,
+    width: '85%',
+  },
+  createToonContainer: {
+    flex: 1,
+  },
+  titleText: {
+    fontFamily: strings.FONT,
+    fontSize: 30,
+    marginTop: 10,
+  },
+  titleContainer: {
+    borderWidth: 4,
+    marginTop: 10,
+  },
+  titleBar: {
+    fontFamily: strings.FONT,
+    fontSize: 20,
+    padding: 10,
+  },
+  listsContainer: {
+    flex: 1,
+    marginTop: 20,
+  },
+  showListContainer: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  listImage: {
+    borderWidth: 4,
+    marginRight: 10,
+  },
+  showListImage: {
+    width: metrics.DEVICE_WIDTH / 5,
+    height: metrics.DEVICE_HEIGHT / 7,
+    resizeMode: 'contain',
+  },
+  listNameContainer: {
+    flex: 1,
+  },
+  listName: {
+    fontFamily: strings.FONT,
+    fontSize: 18,
+    marginTop: 10,
+  },
+  epsNameDate: {
+    fontFamily: strings.FONT,
+    fontSize: 18,
+    opacity: 0.3,
+  },
+  addEpsBtnContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 50,
+  },
+  addEpsBtn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.DARK_GREEN,
+    borderWidth: 4,
+    borderColor: 'rgba(255,255,255,0.7)',
+  },
+  addEpsBtnText: {
+    fontFamily: strings.FONT,
+    color: colors.WHITE,
+    fontSize: 25,
+    padding: 10,
+  },
+});
