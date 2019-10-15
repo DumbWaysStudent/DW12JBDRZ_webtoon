30e80cae6ad494a84c9986e350a458be2a4727af
Egi Ginting <exneval.rayz@gmail.com>
2019-10-15 15:15:14 +0700

    add register implementation

---
+++ server.js
@@ -0,0 +1,24 @@
+//jshint esversion:6
+
+require("dotenv").config();
+
+const express = require("express");
+require("express-group-routes");
+
+const app = express();
+const port = 5000;
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const AuthController = require("./controllers/auth");
+
+app.group("/api/v1", router => {
+  // Public API
+  // Login
+  router.post("/login", AuthController.login);
+  // Register
+  router.post("/register", AuthController.register);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
