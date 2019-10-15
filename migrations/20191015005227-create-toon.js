90406275ba74ca2eda1ff9173c5748b5afd9a8a9
Egi Ginting <exneval.rayz@gmail.com>
2019-10-16 01:23:05 +0700

    add create my episode implementation

---
+++ migrations/20191015005227-create-toon.js
@@ -0,0 +1,51 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("toons", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      genre: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "genres",
+          key: "id"
+        },
+        onUpdate: "cascade",
+        onDelete: "cascade"
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      created_by: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id"
+        },
+        onUpdate: "cascade",
+        onDelete: "cascade"
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("toons");
+  }
+};
