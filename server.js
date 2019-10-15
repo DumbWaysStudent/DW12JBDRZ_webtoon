1e2cf7ce767d5259e3a5c5ed13990a32315ea464
Egi Ginting <exneval.rayz@gmail.com>
2019-10-15 23:01:11 +0700

    add update / detail my webtoon implementation

---
+++ server.js
@@ -0,0 +1,62 @@
+//jshint esversion:6
+
+require("dotenv").config();
+
+const express = require("express");
+require("express-group-routes");
+
+const app = express();
+const port = 5000;
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const AuthController = require("./controllers/auth");
+const ToonsController = require("./controllers/toons");
+
+const { authenticated } = require("./middleware");
+
+app.group("/api/v1", router => {
+  // Public API
+  // Login
+  router.post("/login", AuthController.login);
+  // Register
+  router.post("/register", AuthController.register);
+  // Get all episodes of a toon
+  router.get("/webtoon/:id_webtoon/episodes", ToonsController.showToonEps);
+  // Get all pages of an episode
+  router.get(
+    "/webtoon/:id_webtoon/episode/:id_episode",
+    ToonsController.showToonPages
+  );
+
+  // Private API
+  // Get/Search toons
+  router.get("/webtoons", authenticated, ToonsController.showAllToons);
+  // Get created toons
+  router.get(
+    "/user/:user_id/webtoons",
+    authenticated,
+    ToonsController.showCreatedToons
+  );
+  // Post a created toon
+  router.post(
+    "/user/:user_id/webtoon",
+    authenticated,
+    ToonsController.storeCreatedToon
+  );
+  // Get all episodes of a created toon
+  router.get(
+    "/user/:user_id/webtoon/:webtoon_id/episodes",
+    authenticated,
+    ToonsController.showEpsCreatedToon
+  );
+  // Update my created toon
+  router.put(
+    "/user/:user_id/webtoon/:webtoon_id",
+    authenticated,
+    ToonsController.updateMyCreatedToon
+  );
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
